2fc0b5bbbcc177ec5b870aa02a9ba824bda45f0f
DESKTOP-732ATD8\BLT <164060102@qq.com>
2021-10-21 14:38:14 +0800

    test

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace ConsoleApp1
 {
@@ -6,7 +7,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TimeZoneInfo easternZone = default(TimeZoneInfo);
+            //WINDIWOS
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Console.WriteLine("Eastern Standard Time");
+                easternZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+            }
+            //LINUX
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Console.WriteLine("America/New_York");
+                easternZone = TimeZoneInfo.FindSystemTimeZoneById("America/New_York");
+            }
+            Console.WriteLine(RuntimeInformation.FrameworkDescription);
+            Console.WriteLine(RuntimeInformation.OSDescription);
+            Console.WriteLine(RuntimeInformation.ProcessArchitecture);
+            Console.WriteLine(RuntimeInformation.OSArchitecture);
+            Console.WriteLine("END");
         }
     }
 }
